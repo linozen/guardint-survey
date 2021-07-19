c38bd1d58d7b0203d1ba6abd6cf15e4e896f4b9a
linozen <linus@sehn.tech>
2021-07-19 08:48:54 +0200

    new script

--- generate_profiles.py
+++ generate_profiles.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-import numpy as np
 import pandas as pd
 from pandas_profiling import ProfileReport
 
 # Import dataframes
 df_all = pd.read_pickle("data/all.pkl")
 df_media = pd.read_pickle("data/media.pkl")
+df_civsoc = pd.read_pickle("data/civsoc.pkl")
 
 # Generate profiles
-media_profile = ProfileReport(df_media, config_file="profile.yml")
+all_profile = ProfileReport(df_all, config_file="profiles/profile_all.yml")
+all_profile.to_file("profiles/all.html")
+
+media_profile = ProfileReport(df_media, config_file="profiles/profile_media.yml")
 media_profile.to_file("profiles/media.html")
 
-all_profile = ProfileReport(df_all, config_file="profile.yml")
-all_profile.to_file("profiles/all.html")
+civsoc_profile = ProfileReport(df_civsoc, config_file="profiles/profile_civsoc.yml")
+civsoc_profile.to_file("profiles/civsoc.html")
